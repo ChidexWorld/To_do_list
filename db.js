7f457b4812046f9c6e3c447896d11eaaba8c68f0
ChidexWorld <chiderastanley272@gmail.com>
2024-11-16 19:21:56 +0100

    second commit

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 // Create a connection to the database
 const db = mysql.createConnection({
-  host: "localhost", // Your MySQL host
-  user: "root", // Your MySQL username
-  password: "", // Your MySQL password
-  database: "To_do_app", // Your database name
+  host: process.env.DB_HOST, // MySQL host from .env
+  user: process.env.DB_USER, // MySQL username from .env
+  password: process.env.DB_PASSWORD, // MySQL password from .env
+  database: process.env.DB_NAME, // Database name from .env
 });
 
 // Connect to MySQL
